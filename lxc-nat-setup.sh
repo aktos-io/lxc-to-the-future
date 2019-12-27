bbd524c5b9fd85d20b41a1b758f669336a4dd2a4
Cerem Cem ASLAN <cem@aktos.io>
2019-12-27 21:58:44 +0000

    made port forwardings also work for localhost:PORT on HOST

--- lxc-nat-setup.sh
+++ lxc-nat-setup.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 # This is the address we assigned to our bridge in /etc/network/interfaces
-braddr=10.0.8.1
-iface=eth0
+get_iface_addr(){
+	ip -4 addr show $1 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'
+}
+
+br_iface="lxc-bridge"
+iface="enp0s3"
+
+br_addr=`get_iface_addr $br_iface`
+iface_addr=`get_iface_addr $iface`
 
 # Make sure that the IP forwarding is enabled
 echo 1 > /proc/sys/net/ipv4/ip_forward
+
+sysctl -w net.ipv4.conf.all.route_localnet=1
 
 # Cleanup the iptables
 echo "Cleaning up iptables..."
@@ -12,7 +21,7 @@ iptables -F
 iptables -F -t nat
 
 echo "Adding LXC rules"
-iptables -A FORWARD -i lxc-bridge -s ${braddr}/24 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -i ${br_iface} -s ${br_addr}/24 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A POSTROUTING -t nat -j MASQUERADE
 
@@ -22,11 +31,14 @@ fwd(){
         echo "...forwarding $src to $tgt"
         iptables -t nat -A PREROUTING -i $iface -p tcp --dport $src -j DNAT --to $tgt
         iptables -t nat -A OUTPUT -o lo -d 127.0.0.1 -p tcp --dport $src -j DNAT  --to-destination $tgt
+	iptables -t nat -A OUTPUT -d $iface_addr -p tcp --dport $src -j DNAT --to $tgt
 }
 
+
+
 fwd 80 to 10.0.8.8:80
-fwd 443 to 10.0.8.8:443
-fwd 22 to 10.0.8.9:22 # assign another port to SSH server for this host machine!
+fwd 5984 to 10.0.8.8:5984
+#fwd 22 to 10.0.8.9:22 # assign another port to SSH server for this host machine!
 
 # Testing
 # ---------
